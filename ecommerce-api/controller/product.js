b71514aa2990a58c42f84f508991659811fd5928
Sagar <dev.sagartmg@gmail.com>
2023-11-30 16:07:26 +0545

    filter products

--- ecommerce-api/controller/product.js
+++ ecommerce-api/controller/product.js
@@ -2,27 +2,61 @@ const path = require("path")
 
 const ProductModel = require("../model/Product")
 let getProducts = async (req, res) => {
-    console.log(req.query);
+    console.log("req.query", req.query);
 
     let searchTerm = req.query.searchTerm || ""
-    let page = req.query.page || 1;
-    let perPage = req.query.perPage || 2;
+    let page = parseInt(req.query.page) || 1;
+    let perPage = parseInt(req.query.perPage) || 2;
+    let priceFrom = parseFloat(req.query.priceFrom) || 0
+    let priceTo = parseFloat(req.query.priceTo) || 99999999999
 
-    let sortBy = { createdAt: -1 }
+    let sortBy = { createdAt: -1 } // latest products
+
+    let reqSortBy = req.query.sortBy
+
+    if (reqSortBy == "titleAsc") {
+        sortBy = { title: 1 }
+    } else if (reqSortBy == "titleDesc") {
+        sortBy = { title: -1 }
+    } else if (reqSortBy == "priceAsc") {
+        sortBy = { price: 1 }
+    } else if (reqSortBy == "priceDesc") {
+        sortBy = { price: -1 }
+    } else if (reqSortBy == "recent") {
+        sortBy = { createdAt: -1 }
+    } else if (reqSortBy == "oldest") {
+        sortBy = { createdAt: 1 }
+    }
 
     /* if else ... */
+    // let filterObj = { title: RegExp(searchTerm, "i") }
+    /* mongodb query opetaors $gte, $lte , $and   */
+    // let filterObj = { price:1000 }
+    // let filterObj = { price: { $gte: 100 } }
+    // let filterObj = { price: { $lte: 500 } }
+
+    let filterObj = {
+        $and: [
+            { title: RegExp(searchTerm, "i") },
+            { price: { $gte: priceFrom } },
+            { price: { $lte: priceTo } },
+        ]
+    }
 
     try {
-        let products = await ProductModel.find({ title: RegExp(searchTerm, "i") })
+        let products = await ProductModel.find(filterObj)
             .populate("createdBy")
             .sort(sortBy)
             .skip((page - 1) * perPage)
             .limit(perPage)
 
+        let total = await ProductModel.find(filterObj)
+            .countDocuments()
+
         res.send({
             page,
             perPage,
-            total: "total",
+            total,
             data: products
         })
     }
@@ -38,7 +72,7 @@ let postProducts = async (req, res, next) => {
     let imagePath = null
 
     try {
-        if (req.files?.image) {
+        if (req.files?.image) { // optional chaingin  ?.
             let rootPath = path.resolve()
             let uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
 
@@ -82,7 +116,33 @@ let postProducts = async (req, res, next) => {
     }
 }
 
+
+const updateProducts = async (req, res, next) => {
+    console.log(req.params);
+    try {
+        // update product
+        let product = await ProductModel.findByIdAndUpdate(req.params.id, req.body, {
+            new: true
+        })
+        res.send(product)
+    } catch (err) {
+        next(err)
+    }
+}
+const deleteProduct = async (req, res, next) => {
+    console.log(req.params);
+    try {
+        // update product
+        let product = await ProductModel.findByIdAndDelete(req.params.id)
+        res.send(product)
+    } catch (err) {
+        next(err)
+    }
+}
+
 module.exports = {
     "fetch": getProducts,
     "store": postProducts,
+    "update": updateProducts,
+    "remove": deleteProduct,
 }
--- ecommerce-api/routes/product.js
+++ ecommerce-api/routes/product.js
@@ -8,11 +8,13 @@ const { checkAuthentication } = require("../middleware/auth")
 
 // const product = require("../controller/product") 
 
-const { fetch, store } = require("../controller/product")
+const { fetch, store, update, remove } = require("../controller/product")
 
 router.get('/api/products', fetch)
 
 router.post('/api/products', checkAuthentication, store);
+router.put('/api/products/:id', checkAuthentication, update);
+router.delete('/api/products/:id', checkAuthentication, remove);
 
 router.post('/api/orders', checkAuthentication, (req, res) => {
     res.send("orders created.")
